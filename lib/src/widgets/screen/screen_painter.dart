600a65f695770f176ff2c223515c8fc6a39fac1d
Zeh Fernando <zeh@work.co>
2019-04-12 18:31:42 -0400

    Refactor: drawing is done with a drawAtlas() call, not drawImage()

--- lib/src/widgets/screen/screen_painter.dart
+++ lib/src/widgets/screen/screen_painter.dart
@@ -16,31 +16,76 @@ class ScreenPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    Rect rect = Offset.zero & size; // Rect.fromPoints(Offset(0.0, 0.0), Offset(size.));
-    var paint = Paint();
-    paint.color = Color(0xff000000);
-
-    canvas.drawRect(
-      rect,
-      paint,
-    );
+    Rect rect = Offset.zero & size;
+    int defaultBackgroundColorCode = 0;
+    Paint backgroundPaint = Paint();
+    backgroundPaint.color = colors.get(defaultBackgroundColorCode);
+    canvas.drawRect(rect, backgroundPaint);
 
     if (charset.imageInfo != null && charset.imageInfo.image != null) {
-      print("PAINTING WITH IMAGE");
-      //var charPaint = Paint();
-      //charPaint.color = Color(0xffffffff);
-      //canvas.drawImageRect(_imageInfo.image, Rect.fromPoints(Offset.zero, Offset(40, 40)), Rect.fromPoints(Offset.zero,  Offset(40, 40)), charPaint);
       if (document != null) {
-        for (var row = 0; row < document.height; row++) {
-          for (var col = 0; col < document.width; col++) {
-            // TODO: this is temporary and not performant
-            int charCode = document.getChar(col, row);
-            // TODO: this shouldn't be needed, optimize somewhere else
-            if (charCode != 32) {
-              _placeCharacter(canvas, charCode, col, row);
+        final stopwatch = Stopwatch()..start();
+        final double charWidth = charset.charWidth.toDouble();
+        final double charHeight = charset.charHeight.toDouble();
+        final int charCols = charset.imageInfo.image.width ~/ charWidth;
+
+        int rows = document.height;
+        int cols = document.width;
+        int size = rows * cols;
+        List<Color> charColors = List<Color>(size);
+        List<RSTransform> charTransforms = List<RSTransform>(size);
+        List<Rect> charRects = List<Rect>(size);
+
+        // Initializers to reduce GC
+        int charCode;
+        int foregroundColor, backgroundColor;
+        int pos;
+        int charCol, charRow;
+
+        double charPosFromX, charPosFromY, charPosToX, charPosToY;
+
+        for (var row = 0; row < rows; row++) {
+          for (var col = 0; col < cols; col++) {
+            charCode = document.getChar(col, row);
+            foregroundColor = document.getForegroundColor(col, row);
+            backgroundColor = document.getBackgroundColor(col, row);
+            pos = row * cols + col;
+
+            charCol = charCode % charCols;
+            charRow = charCode ~/ charCols;
+
+            charPosFromX = charCol * charWidth;
+            charPosFromY = charRow * charHeight;
+            charPosToX = col * charWidth;
+            charPosToY = row * charHeight;
+
+            // Paints background space
+            if (backgroundColor != defaultBackgroundColorCode) {
+              backgroundPaint.color = colors.get(backgroundColor);
+              canvas.drawRect(Rect.fromLTWH(charPosToX, charPosToY, charWidth, charHeight), backgroundPaint);
             }
+
+            // Adds the foreground character to an atlas
+            // TODO: detect unneeded foreground paints
+            //if (charCode != 32)
+            charTransforms[pos] = RSTransform.fromComponents(rotation: 0, scale: 1, anchorX: 0, anchorY: 0, translateX: charPosToX, translateY: charPosToY);
+            charRects[pos] = Rect.fromLTWH(charPosFromX, charPosFromY, charWidth, charHeight);
+            charColors[pos] = colors.get(foregroundColor);
           }
         }
+
+        // Draws characters
+        Paint paint = Paint();
+        canvas.drawAtlas(
+          charset.imageInfo.image,
+          charTransforms,
+          charRects,
+          charColors,
+          BlendMode.modulate,
+          null,
+          paint,
+        );
+        print('Painting for ${rows * cols} characters executed in ${stopwatch.elapsedMicroseconds / 1000}ms');
       } else {
         print("Missing document?!");
       }
@@ -54,29 +99,4 @@ class ScreenPainter extends CustomPainter {
 
   @override
   bool shouldRebuildSemantics(ScreenPainter oldDelegate) => false;
-
-  _placeCharacter(Canvas canvas, int char, int col, int row) {
-    int charWidth = charset.charWidth;
-    int charHeight = charset.charHeight;
-    int charCols = charset.imageInfo.image.width ~/ charWidth;
-
-    int charCol = char % charCols;
-    int charRow = char ~/ charCols;
-
-    Paint charPaint = Paint();
-    charPaint.color = Color(0xffffffff);
-
-    var charPosFrom = Offset(charCol * charWidth.toDouble(), charRow * charHeight.toDouble());
-    var charPosTo = Offset(col * charWidth.toDouble(), row * charHeight.toDouble());
-    var charSize = Offset(charWidth.toDouble(), charHeight.toDouble());
-
-    charPaint.colorFilter = colors.getStampColorFilter(2);
-    canvas.drawRect(Rect.fromPoints(charPosTo, charPosTo + charSize), charPaint);
-
-    charPaint.colorFilter = colors.getStampColorFilter(10);
-    canvas.drawImageRect(charset.imageInfo.image, Rect.fromPoints(charPosFrom, charPosFrom + charSize), Rect.fromPoints(charPosTo, charPosTo + charSize), charPaint);
-
-    // TODO: use atlas for speed
-    //canvas.drawAtlas(atlas, transforms, rects, colors, blendMode, cullRect, paint)
-  }
 }
