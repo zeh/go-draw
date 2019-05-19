6dcfc22e88ed439f252889bfdc54601e960f1df7
Zeh Fernando <zeh@zehfernando.com>
2019-05-19 15:52:19 -0400

    _toolbar

--- lib/src/widgets/tool_bar/tool_bar.dart
+++ lib/src/widgets/tool_bar/tool_bar.dart
@@ -1,5 +1,7 @@
 import "package:flutter/widgets.dart";
+import "package:go_draw/src/utils/animate.dart";
 
+import "background.dart";
 import "tool_button.dart";
 
 class ToolBar extends StatefulWidget {
@@ -21,31 +23,53 @@ enum Tools {
   Share,
 }
 
-class _ToolBarState extends State<ToolBar> {
+class _ToolBarState extends State<ToolBar> with SingleTickerProviderStateMixin {
+
+  AniMate _animate;
+
   @override
   void initState() {
     super.initState();
+
+    _animate = AniMate.create(
+      ticker: this,
+      state: this,
+      duration: 3000,
+      curve: Curves.easeOut,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xffe0e0e0),
-      child: Row(
+      height: 200,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
         children: [
-          ToolButton(
-            assetName: "assets/tools/i-cursor.svg",
-            onTap: () => widget.onTap(Tools.Text),
+          // Row(
+          //   children: [
+          //     ToolButton(
+          //       assetName: "assets/tools/i-cursor.svg",
+          //       onTap: () => widget.onTap(Tools.Text),
+          //     ),
+          //     ToolButton(
+          //       assetName: "assets/tools/folder-open.svg",
+          //       onTap: () => widget.onTap(Tools.File),
+          //     ),
+          //     ToolButton(
+          //       assetName: "assets/tools/share-alt.svg",
+          //       onTap: () => widget.onTap(Tools.Share),
+          //     ),
+          //   ],
+          // ),
+          Background(
+            carveDepth: _animate.value * 40,
+            carvePosition: 0.25,
+            circleDepth: 0,
+            circleRadius: _animate.value * 36,
           ),
-          ToolButton(
-            assetName: "assets/tools/folder-open.svg",
-            onTap: () => widget.onTap(Tools.File),
-          ),
-          ToolButton(
-            assetName: "assets/tools/share-alt.svg",
-            onTap: () => widget.onTap(Tools.Share),
-          ),
-        ],
+        ]
       ),
     );
   }
@@ -53,5 +77,6 @@ class _ToolBarState extends State<ToolBar> {
   @override
   dispose() {
     super.dispose();
+    _animate?.dispose();
   }
 }
