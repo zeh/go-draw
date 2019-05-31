cf7be7975d3bc014812883b06d8626317d02f7f1
Zeh Fernando <zeh@work.co>
2019-05-31 15:41:29 -0400

    Fix: charset loading works again with new image streaming listener class

--- lib/src/data/screen/charset/screen_charset.dart
+++ lib/src/data/screen/charset/screen_charset.dart
@@ -3,6 +3,7 @@ import "package:flutter/widgets.dart";
 class ScreenCharset {
   ImageStream _imageStream;
   ImageInfo _imageInfo;
+  ImageStreamListener _imageStreamListener;
   final int charWidth;
   final int charHeight;
 
@@ -16,8 +17,9 @@ class ScreenCharset {
     @required this.charWidth,
     @required this.charHeight
   }) {
+    _imageStreamListener = new ImageStreamListener(_updateImage);
     _imageStream = map.resolve(createLocalImageConfiguration(context));
-    _imageStream.addListener(_updateImage);
+    _imageStream.addListener(_imageStreamListener);
   }
 
 
@@ -31,7 +33,7 @@ class ScreenCharset {
 	// PUBLIC INTERFACE -----------------------------------------------------------------------------------------------
 
   void dispose() {
-    _imageStream.removeListener(_updateImage);
+    _imageStream.removeListener(_imageStreamListener);
   }
 
 
