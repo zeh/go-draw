21bea84a97a657712b5ac194bb45c9d13d5fec7e
Zeh Fernando <zeh@work.co>
2019-05-31 17:03:13 -0400

    Feature: initialize app with performance tracking enabled if allowed

--- lib/src/app_runner.dart
+++ lib/src/app_runner.dart
@@ -3,6 +3,7 @@ import "dart:async";
 import "package:flutter/widgets.dart";
 import "package:go_draw/src/config/config.dart";
 import "package:go_draw/src/go_draw.dart";
+import "package:go_draw/src/utils/perf.dart";
 import "package:go_draw/src/utils/sentry_tracking.dart";
 import "package:go_draw/src/utils/analytics.dart";
 
@@ -14,6 +15,9 @@ class AppRunner {
       // Initialize analytics
       Analytics.setEnabled(Config.get().AnalyticsCollectionEnabled);
       Analytics.setLogEnabled(Config.get().AnalyticsLogEnabled);
+
+      // Initialize performance tracking
+      Perf.setEnabled(Config.get().AnalyticsPerformanceEnabled);
 
       if (Config.get().SentryEnabled) {
         // Initialize with error interception
