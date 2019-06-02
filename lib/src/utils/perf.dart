c3e31700a1b2406a460729fbcc07fe0c7d61e86a
Zeh Fernando <zeh@zehfernando.com>
2019-06-02 13:00:05 -0400

    Feature: added a helper class to track performance

---
+++ lib/src/utils/perf.dart
@@ -0,0 +1,20 @@
+import "package:firebase_performance/firebase_performance.dart";
+
+// For more info: https://pub.dev/packages/firebase_performance
+
+class Perf {
+  static init() {
+
+  }
+
+  static Future<void> setEnabled(bool enabled) async {
+    await FirebasePerformance.instance.setPerformanceCollectionEnabled(enabled);
+  }
+
+  // TODO: track performance
+  // Example:
+  // final Trace myTrace = FirebasePerformance.instance.newTrace("test_trace");
+  // myTrace.start();
+  // myTrace.incrementMetric("test_hit", 1);
+  // myTrace.stop();
+}
